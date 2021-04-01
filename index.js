b077d285881674a38ad3024c3ff12e4ea321020c
Adithya Mallya <SlaYpoint>
2021-04-01 11:21:58 +0530

    added function to store items locally

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ let form = document.querySelector('#addForm');
 let itemList = document.querySelector('#itemList');
 let filterItem = document.querySelector('#filter');
 
+// Load items
+document.addEventListener('DOMContentLoaded', function(e){
+    let items;
+    if(localStorage.getItem('items') === null){
+        items = [];
+    }else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+
+    items.forEach((item)=>{
+        let li = document.createElement('li');
+        li.className = 'list--item';
+        li.appendChild(document.createTextNode(item));
+
+        let delBtn = document.createElement('button');
+        delBtn.className = 'delete__button';
+        delBtn.appendChild(document.createTextNode('X'));
+        li.appendChild(delBtn);
+
+        itemList.appendChild(li);
+    });
+
+});
+
 // Add items
 form.addEventListener('submit', function (e) {
     e.preventDefault();//to prevent form submiting
@@ -22,6 +46,8 @@ form.addEventListener('submit', function (e) {
 
     itemList.appendChild(li);
 
+    storeItemLocally(newItem.value);
+
     // console.log(li);
 
 });
@@ -33,6 +59,10 @@ itemList.addEventListener('click', function(e){
         if(confirm('Sure you want to remove this item ?')){
             let li = e.target.parentElement;//grabs the li element
             itemList.removeChild(li);
+            // console.log(li);
+
+            // remove from localStorage as well
+            removeItemLocally(e.target.parentElement);
         }
     }
 });
@@ -53,3 +83,35 @@ filterItem.addEventListener('keyup', function(e){
         }
     })
 });
+
+// Store locally 
+const storeItemLocally = (item) => {
+    let items;
+    if(localStorage.getItem('items') === null){
+        items = [];
+    }else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+
+    items.push(item);
+    // Save to localStorage
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+// Remove from localStorage
+const removeItemLocally = (item) => {
+    let items;
+    if(localStorage.getItem('items') === null){
+        items = [];
+    }else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+
+    items.forEach((itemName, index) =>{
+        if(item.textContent === itemName ){
+            items.splice(index, 1);
+        }
+    });
+    //re-store the array of items
+    localStorage.setItem('items', JSON.stringify(items));
+}
